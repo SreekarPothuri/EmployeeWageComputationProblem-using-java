ff1b04fdae5ba611a934c8dc7a2fdbf1f882310d
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-22 15:20:02 +0530

    UC-4:Added

--- EmpWageProblem.java
+++ EmpWageProblem.java
@@ -13,22 +13,26 @@ public class EmpWageProblem
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		double EmpCheck = Math.floor(Math.random() * 10) % 3;
+		int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
 
-		if(EmpCheck == 1)
+		switch(CheckEmp)
 		{
-			System.out.println("Full Time Employee and is present");
-			DailyWageSalary=WagePerHr*HrsPerDay_fullTime;
-			System.out.println("Salary : "+DailyWageSalary);
-		}
-		else if(EmpCheck ==2)
-		{
-			System.out.println("Part Time Employee and is present");
-			DailyWageSalary=WagePerHr*HrsPerDay_PartTime;
-			System.out.println("Salary : "+DailyWageSalary);
-		}
-		else
-		{
-			System.out.println("Employee is absent");
+			case 0:
+				System.out.println("Employee is absent");
+				break;
+			case 1:
+				System.out.println("FullTime Employee and he is present");
+				DailyWageSalary=WagePerHr*HrsPerDay_fullTime;
+				System.out.println("Salary : "+DailyWageSalary);
+				break;
+			case 2:
+				System.out.println("Part Time Employee and he is present");
+				DailyWageSalary=WagePerHr*HrsPerDay_PartTime;
+				System.out.println("Salary : "+DailyWageSalary);
+				break;
+			default:
+				System.out.println("Invalid");
+				break;
 		}
 	}
 }
