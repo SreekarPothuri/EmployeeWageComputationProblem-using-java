2de681a5d10425aa2c2076579224b5c46487c363
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-22 11:28:25 +0530

    UC-1:Attendance check Added

--- EmpWageProblem.java
+++ EmpWageProblem.java
@@ -3,5 +3,10 @@ public class EmpWageProblem
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Employee Wage Computation Program");
+		double EmpCheck = Math.floor(Math.random() * 10) % 2;
+		if(EmpCheck == 1)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 }
