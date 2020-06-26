25a46eb388c45420ce030e835652b425ef6fa85d
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-26 12:51:15 +0530

    UC-9:Ability to save the Total Wage for Each Company

--- EmpWageProblem.java
+++ EmpWageProblem.java
@@ -3,8 +3,21 @@ public class EmpWageProblem {
         public static final int is_full_time=1;
         public static final int is_part_time=2;
 
-        public static int computeEmpWage(String company, int WagePerHr, int Max_WorkingDaysPerMonth, int Max_WorkingHrsPerMonth) {
-                int HrsPerDay=0;
+	private final String company;
+	private final int WagePerHr;
+	private final int Max_WorkingDaysPerMonth;
+	private final int Max_WorkingHrsPerMonth;
+	private int MonthlyWage;
+
+	public EmpWageProblem(String company, int WagePerHr, int Max_WorkingDaysPerMonth, int Max_WorkingHrsPerMonth) {
+		this.company = company;
+		this.WagePerHr = WagePerHr;
+		this.Max_WorkingDaysPerMonth = Max_WorkingDaysPerMonth;
+		this.Max_WorkingHrsPerMonth = Max_WorkingHrsPerMonth;
+	}
+
+        public void computeEmpWage() {
+		int HrsPerDay=0;
                 int WorkingHrs=0;
                 int Day=0;
 
@@ -28,16 +41,30 @@ public class EmpWageProblem {
                         WorkingHrs += HrsPerDay;
                         System.out.println("\t"+"Emp Hr : " +HrsPerDay);
                 }
-                int MonthlyWage = WorkingHrs * WagePerHr;
-                System.out.println("******Total Emp Wage for Company: " +company+ " is "+MonthlyWage+"**************");
-                return MonthlyWage;
+                MonthlyWage = WorkingHrs * WagePerHr;
         }
 
+	@Override
+	public String toString() {
+		return "*****Total Emp Wage for Company " +company+ " is "+MonthlyWage+"*****";
+	}
+
         public static void main(String args[]) {
-                computeEmpWage("PrimeVideo",10,2,10);
-                computeEmpWage("Netflix",20,4,15);
-                computeEmpWage("BridgeLabz",30,6,20);
-                computeEmpWage("Amazon",40,8,25);
-                computeEmpWage("Microsoft",50,10,30);
+		EmpWageProblem TCS = new EmpWageProblem("TCS",10,2,10);
+		EmpWageProblem Infosys = new EmpWageProblem("Infosys",20,4,15);
+		EmpWageProblem BridgeLabz = new EmpWageProblem("BridgeLabz",30,6,20);
+		EmpWageProblem Amazon = new EmpWageProblem("Amazon",40,8,25);
+		EmpWageProblem Netflix = new EmpWageProblem("Netflix",50,10,30);
+
+                TCS.computeEmpWage();
+		System.out.println(TCS);
+                Infosys.computeEmpWage();
+		System.out.println(Infosys);
+                BridgeLabz.computeEmpWage();
+		System.out.println(BridgeLabz);
+                Amazon.computeEmpWage();
+		System.out.println(Amazon);
+                Netflix.computeEmpWage();
+		System.out.println(Netflix);
         }
 }
