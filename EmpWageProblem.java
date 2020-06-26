1eeb6ba6d96ec66788909626a24eea3afe1b741a
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-26 12:22:54 +0530

    UC-8:Ability to compute EmployeeWage for multiple companies

--- EmpWageProblem.java
+++ EmpWageProblem.java
@@ -1,106 +1,43 @@
-import java.util.Scanner;
-public class EmpWageProblem
-{
-	public static final int is_full_time=1;
-	public static final int is_part_time=2;
-	public static int WagePerHr;
-	public static int Max_WorkingHrsPerMonth;
-	public static int Max_WorkingDaysPerMonth;
-	public static int HrsPerDay;
-	public static int Day;
-	static int x=0;
-	static void EmpCompany() {
-		String[] CompanyName={"TCS","Infosys","BridgeLabz","Amazon","Netflix"};
-		for(int i=0; i<CompanyName.length; i++){
-			x=x+1;
-			System.out.println(x+"."+CompanyName[i]);
-		}
-		Scanner Id = new Scanner(System.in);
-		System.out.println("Enter CompanyId");
-		int CompanyId = Id.nextInt();
-		switch(CompanyId){
-			case 1:
-				System.out.println("-------------Welcome to TCS-------------------");
-				WagePerHr=10;
-				Max_WorkingHrsPerMonth=180;
-				Max_WorkingDaysPerMonth=26;
-				break;
-			case 2:
-				System.out.println("-------------Welcome to Infosys----------------");
-				WagePerHr=20;
-				Max_WorkingHrsPerMonth=185;
-				Max_WorkingDaysPerMonth=25;
-				break;
-			case 3:
-				System.out.println("--------------Welcome to BridgeLabz------------");
-				WagePerHr=30;
-				Max_WorkingHrsPerMonth=190;
-				Max_WorkingDaysPerMonth=24;
-				break;
-			case 4:
-				System.out.println("--------------Welcome to Amazon-----------------");
-				WagePerHr=40;
-				Max_WorkingHrsPerMonth=195;
-				Max_WorkingDaysPerMonth=23;
-				break;
-			case 5:
-				System.out.println("--------------Welcome to Netflix-----------------");
-				WagePerHr=50;
-				Max_WorkingHrsPerMonth=200;
-				Max_WorkingDaysPerMonth=22;
-				break;
-			default:
-				System.out.println("Invalid CompanyId");
-				break;
-		}
-	}
+public class EmpWageProblem {
 
-	static void EmpWage() {
+        public static final int is_full_time=1;
+        public static final int is_part_time=2;
 
-		double EmpCheck = Math.floor(Math.random() * 10) % 3;
-		int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
+        public static int computeEmpWage(String company, int WagePerHr, int Max_WorkingDaysPerMonth, int Max_WorkingHrsPerMonth) {
+                int HrsPerDay=0;
+                int WorkingHrs=0;
+                int Day=0;
 
-		switch(CheckEmp)
-		{
-			case 0:
-				System.out.println("Day#"+Day + " " + "Employee is absent");
-				HrsPerDay=0;
-				break;
-			case 1:
-				System.out.println("Day#"+Day +" " + "FullTime Employee and he is present");
-				HrsPerDay=8;
-				break;
-			case 2:
-				System.out.println("Day#"+Day +" " +"Part Time Employee and he is present");
-				HrsPerDay=4;
-				break;
-			default:
-				System.out.println("Invalid");
-				break;
-			}
-	}
+                while(WorkingHrs <= Max_WorkingHrsPerMonth && Day < Max_WorkingDaysPerMonth) {
+                        Day++;
+                        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                        switch(empCheck) {
+                                case is_full_time:
+					System.out.print("Day#"+Day+" "+"FullTime Employee and he is present");
+                                        HrsPerDay=8;
+                                        break;
+                                case is_part_time:
+					System.out.print("Day#"+Day+" "+"PartTime Employee and he is present");
+                                        HrsPerDay=4;
+                                        break;
+                                default:
+					System.out.print("Day#"+Day+" "+"Employee is Absent");
+                                        HrsPerDay=0;
+                                        break;
+                        }
+                        WorkingHrs += HrsPerDay;
+                        System.out.println("\t"+"Emp Hr : " +HrsPerDay);
+                }
+                int MonthlyWage = WorkingHrs * WagePerHr;
+                System.out.println("******Total Emp Wage for Company: " +company+ " is "+MonthlyWage+"**************");
+                return MonthlyWage;
+        }
 
-	public static void main(String args[])
-	{
-		int DailyWage;
-		int MonthlyWage=0;
-		int WorkingHrs=0;
-
-		EmpCompany();
-		System.out.println();
-		for(Day=1; Day<=Max_WorkingDaysPerMonth; Day++) {
-			if(WorkingHrs <=Max_WorkingHrsPerMonth) {
-				EmpWage();
-				DailyWage=WagePerHr*HrsPerDay;
-				System.out.println("Salary for day : "+DailyWage);
-				MonthlyWage=MonthlyWage+DailyWage;
-				System.out.println("Total Salary till date : "+MonthlyWage);
-				WorkingHrs=WorkingHrs+HrsPerDay;
-				System.out.println();
-			}
-		}
-		System.out.println("Toatal Salary for month : " +MonthlyWage);
-		System.out.println("Toatal Working Hours : " +WorkingHrs);
-	}
+        public static void main(String args[]) {
+                computeEmpWage("PrimeVideo",10,2,10);
+                computeEmpWage("Netflix",20,4,15);
+                computeEmpWage("BridgeLabz",30,6,20);
+                computeEmpWage("Amazon",40,8,25);
+                computeEmpWage("Microsoft",50,10,30);
+        }
 }
-
